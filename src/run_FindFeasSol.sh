851ad502ebb2092b47ab020832c195256dee09c1
marzb93 <bakhshimarzieh93@gmail.com>
2024-02-23 11:57:04 -0500

    updated version

--- src/run_FindFeasSol.sh
+++
@@ -1,9 +0,0 @@
-m=(1000 2000 4000 6000 8000 10000 20000 40000 60000 80000 100000)
-n=(50 100)
-for i in "${m[@]}"; do
-  for j in "${n[@]}"; do
-    echo "Running FindFeasSol.py with argument $i $j"
-    python3 FindFeasSol.py "$i" "$j"
-    echo "------------------------"
-  done  
-done
